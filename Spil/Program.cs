09e4909f048078f38a40c11c42e022b13ae76507
Pjinky <enevoldsen.ae@hotmail.com>
2020-03-23 11:27:32 +0100

    Added highscore variable and stuff

--- Spil/Program.cs
+++ Spil/Program.cs
@@ -7,6 +7,7 @@ namespace Spil
         static Random rnd = new Random();
         static int min = 0;
         static int max = 10;
+        static int highscore = 0;
 
         static void Main(string[] args)
         {
@@ -14,12 +15,13 @@ namespace Spil
             bool exit = false;
             do
             {
-
+                int points = 0;
                 do
                 {
                     int result = rnd.Next(min, max);
                     int attempts = 3;
                     int guess = 0;
+                    
                     Console.Clear();
                     Console.WriteLine("Gæt et tal mellem {0}-{1}", min, max);
                     do
@@ -27,20 +29,28 @@ namespace Spil
                         String input = Console.ReadLine();
                         guess = int.Parse(input);
                         if (guess > result)
-                            Console.WriteLine("Dit gæt (" + guess + ") er for højt." + attempts);
+                            Console.WriteLine("Dit gæt (" + guess + ") er for højt.");
                         if (guess < result)
-                            Console.WriteLine("Dit gæt (" + guess + ") er for lavt." + attempts);
+                            Console.WriteLine("Dit gæt (" + guess + ") er for lavt.");
                         attempts--;
-                    } while (guess != result | attempts > 0);
+                    } while (guess != result & attempts > 0);
                     if(guess == result)
                     {
+                        points++;
                         Console.WriteLine("Tillykke, du gættede det rigtige tal {0}", result);
                     }
                     else
                     {
+                        if (points > highscore)
+                        {
+                            highscore = points;
+                        }
+                        points = 0;
                         Console.WriteLine("Desværre, du har opbrugt alle dine forsøg.");
+
                     }
                     Console.WriteLine();
+                    Console.WriteLine("Du har " + points + " point og der er en highscore på " + highscore);
                     Console.Write("Tryk en vilkårlig tast for at prøve igen, tryk R for at nulstille alt eller ESC for at lukke ned");
                     ConsoleKey ipt = Console.ReadKey().Key;
                     if (ipt == ConsoleKey.Escape)
@@ -51,6 +61,10 @@ namespace Spil
                     if(ipt == ConsoleKey.R)
                     {
                         restart = true;
+                        if (points > highscore)
+                        {
+                            highscore = points;
+                        }
                     }
                 } while (!restart);
                 restart = false;
